84db8e1b9992f81a9111fef5368bd2a921b0d3df
jassem <mohamedjassemdebbich@gmail.com>
2024-01-20 11:32:17 +0100

    Committing local changes before merge

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -51,7 +51,7 @@ const ChapterIdPage = async ({
   return (
     <>
     
-      <div className="p-6">
+    <div className="p-6">
         <div className="flex items-center justify-between">
           <div className="w-full">
             <Link
@@ -59,15 +59,15 @@ const ChapterIdPage = async ({
               className="flex items-center text-sm hover:opacity-75 transition mb-6"
             >
               <ArrowLeft className="h-4 w-4 mr-2" />
-              Retour à la configuration du cours
+              Back to course setup
             </Link>
             <div className="flex items-center justify-between w-full">
               <div className="flex flex-col gap-y-2">
                 <h1 className="text-2xl font-medium">
-                Création de chapitre
+                  Chapter Creation
                 </h1>
                 <span className="text-sm text-slate-700">
-                  Remplissez tous les champs {completionText}
+                  Complete all fields {completionText}
                 </span>
               </div>
             </div>
@@ -79,7 +79,7 @@ const ChapterIdPage = async ({
               <div className="flex items-center gap-x-2">
                 <IconBadge icon={LayoutDashboard} />
                 <h2 className="text-xl">
-                  Personnalisez votre chapitre
+                  Customize your chapter
                 </h2>
               </div>
               <ChapterTitleForm
@@ -93,30 +93,34 @@ const ChapterIdPage = async ({
                 chapterId={params.chapterId}
               />
             </div>
-            <div className="flex items-center gap-x-2">
-              <IconBadge icon={Eye} />
-              <h2 className="text-xl">
-                Paramètres d'accès
-              </h2>
-            </div>
-            <ChapterAccessForm
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={Eye} />
+                <h2 className="text-xl">
+                  Access Settings
+                </h2>
+              </div>
+              <ChapterAccessForm
                 initialData={chapter}
                 courseId={params.courseId}
                 chapterId={params.chapterId}
               />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={Video} />
+              <h2 className="text-xl">
+                Add a video
+              </h2>
+            </div>
+            <ChapterVideoFrom
+              initialData={chapter}
+              chapterId={params.chapterId}
+              courseId={params.courseId}
+            />
           </div>
         </div>
-        <div className="flex items-center gap-x-2">
-          <IconBadge icon={Video} />
-          <h2 className="text-xl">
-            Ajouter un video
-          </h2>
-        </div>
-        <ChapterVideoFrom 
-          initialData={chapter}
-          chapterId={params.chapterId}
-          courseId={params.courseId}
-        />
       </div>
     </>
     
