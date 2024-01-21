8b868f7c42ede3d5aa41a1f1d3076d75edbdddbf
Talentn <alaghabi98@gmail.com>
2024-01-21 18:06:59 +0100

    ss

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -6,7 +6,7 @@ export async function POST (
     req:Request,
     ) {
     try {
-        const {userId} = auth (); 
+        const {userId} = auth(); 
         const {title} = await req.json();
 
         if (!userId) {
