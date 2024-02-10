6dccde3750bf03999b014ce19f391368936f132f
jassem <mohamedjassemdebbich@gmail.com>
2024-02-10 21:27:47 +0100

    Last reunion

--- app/(dashboard)/(routes)/(root)/_components/info-card.tsx
+++ app/(dashboard)/(routes)/(root)/_components/info-card.tsx
@@ -26,7 +26,7 @@ export const InfoCard = ({
           {label}
         </p>
         <p className="text-gray-500 text-sm">
-          {numberOfItems} {numberOfItems === 1 ? "Course" : "Cours"}
+          {numberOfItems} {numberOfItems === 1 ? "Course" : "Bac"}
         </p>
       </div>
     </div>
