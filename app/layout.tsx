4570a6d3d56847e68c949b377a76502fdf35b9af
Ala Ghabi <91420708+Talentn@users.noreply.github.com>
2024-03-18 16:06:02 +0100

    update name

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Noble',
+  title: 'Nobel',
   description: 'Description a faire',
 }
  
