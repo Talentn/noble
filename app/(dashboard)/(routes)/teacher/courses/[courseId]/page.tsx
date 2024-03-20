88ce74e7719f3ecac76de1374e9e65f50f9e79d9
Ala Ghabi <91420708+Talentn@users.noreply.github.com>
2024-03-20 16:13:46 +0100

    Update page.tsx

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -13,7 +13,6 @@ import { ChaptersForm } from "./_components/chapters-form";
 import { Banner } from "@/components/banner";
 import { Actions } from "./_components/actions";
 
-
 const CourseIdPage = async ({
     params
 }: {
@@ -170,9 +169,6 @@ const CourseIdPage = async ({
             </div>
             
         </div>
-        <div className="w-full bottom-0">
-          <Footer/>
-      </div>
         
         </>
         
