13d580046b90d54a33400e25e9d428eda6d10ec5
Ala Ghabi <91420708+Talentn@users.noreply.github.com>
2024-03-20 16:19:35 +0100

    Update page.tsx

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -38,7 +38,7 @@ const SearchPage = async ({
       <div className="px-6 pt-6 md:hidden md:mb-0 block">
       <SearchInput />
       </div>
-      <div className="p-6 space-y-4 content-container">
+      <div className="p-6 space-y-4">
         <Categories
           items={categories}
         />
@@ -49,7 +49,6 @@ const SearchPage = async ({
             <Footer/>
         </footer>
       </div>
-
     </>
    );
 }
