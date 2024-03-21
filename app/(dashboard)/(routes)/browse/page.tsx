0b243912f6a8d551a7a9b72a66bd5b8c3620a326
Ala Ghabi <91420708+Talentn@users.noreply.github.com>
2024-03-21 10:08:51 +0000

    footer fix

--- app/(dashboard)/(routes)/browse/page.tsx
+++ app/(dashboard)/(routes)/browse/page.tsx
@@ -21,6 +21,7 @@ export default async function Dashboard() {
   } = await getDashboardCourses(userId);
 
   return (
+    
     <div className="p-6 space-y-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
        <InfoCard
@@ -39,12 +40,12 @@ export default async function Dashboard() {
         items={[...coursesInProgress, ...completedCourses]}
       />
       <div>
+        </div>
         <footer>
             <Footer/>
         </footer>
       </div>
-    </div>
 
-      
   )
 }
+
