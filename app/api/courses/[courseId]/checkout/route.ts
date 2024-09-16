94717fcf6931557d646374acc9acf2b376c42328
Talentn <alaghabi98@gmail.com>
2024-09-16 21:37:23 +0100

    fixxx

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -78,8 +78,8 @@ export async function POST(
             customer: stripeCustomer.stripeCustomerId,
             line_items,
             mode: "payment",
-            success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}/success=1`,
-            cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}/canceled=1`,
+            success_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?success=1`,
+            cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/courses/${course.id}?canceled=1`,
             metadata: {
                 userId: user.id,
                 courseId: course.id,
