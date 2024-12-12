d659b16df5e398d3b54f0a05b72c0004f5eec6dc
Ala Ghabi <91420708+Talentn@users.noreply.github.com>
2024-12-12 15:00:54 +0100

    Heyyyeye

--- app/api/payment/confirm/route.ts
+++ app/api/payment/confirm/route.ts
@@ -14,15 +14,6 @@ export async function POST(req: NextRequest) {
             return NextResponse.json({ success: false, message: "Missing required parameters." }, { status: 400 });
         }
 
-        // Load the CA bundle
-        const caBundlePath = path.join(process.cwd(), 'certs', 'STAR_nobel_tn.ca-bundle');
-        const caBundle = fs.readFileSync(caBundlePath);
-
-        // Create an HTTPS agent with the CA bundle
-        const agent = new https.Agent({
-            ca: caBundle,
-        });
-
         // Prepare query parameters
         const queryParams = new URLSearchParams({
             userName: process.env.CLICTOPAY_USER!,
@@ -31,10 +22,7 @@ export async function POST(req: NextRequest) {
             language: "en",
         }).toString();
 
-        // Make GET request using Axios with the custom agent
-        const response = await axios.get(`https://ipay.clictopay.com/payment/rest/getOrderStatus.do?${queryParams}`, {
-            httpsAgent: agent,
-        });
+        const response = await axios.get(`https://ipay.clictopay.com/payment/rest/getOrderStatus.do?${queryParams}`
 
         console.log('ClicToPay API Response Status:', response.status, response.statusText);
         console.log("Payment Status Data:", response.data);
