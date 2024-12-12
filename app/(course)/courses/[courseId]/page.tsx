9c2d664f9ccbf69043bbfaffba7e1bc6d2772d9a
Ala Ghabi <91420708+Talentn@users.noreply.github.com>
2024-12-12 13:21:58 +0100

    Update page.tsx

--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -2,6 +2,9 @@ import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 import PaymentStatusHandler from "@/components/PaymentStatusHandler";
+import https from "https";
+import fs from "fs";
+import path from "path";
 
 interface CourseIdPageProps {
     params: { courseId: string };
@@ -42,10 +45,17 @@ const CourseIdPage = async ({ params, searchParams }: CourseIdPageProps) => {
 
     let paymentStatus = "";
 
+    const caBundlePath = path.join(process.cwd(), "certs", "STAR_nobel_tn.ca-bundle");
+    const caBundle = fs.readFileSync(caBundlePath, "utf-8");
+
+    const httpsAgent = new https.Agent({
+    ca: caBundle,
+});
     if (status === "1" && orderId) {
         try {
             // Confirm payment by calling your server-side payment verification API
             const response = await fetch(`${process.env.NEXT_PUBLIC_APP_URL}/api/payment/confirm`, {
+                agent: httpsAgent,
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
